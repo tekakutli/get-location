a51c225b93f5f3f6b5841e040283d3da77c9133b
tlani <familyquerida@gmail.com>
2021-04-18 23:35:45 -0500

    working

---
+++ get-location.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+GETLOCATION=../get-location/
+FILE=$(echo $1 | rev | cut -d'/' -f 1 | rev)
+FOLDER=../api-location-$FILE
+mkdir -p $FOLDER
+cd $FOLDER
+
+rm location
+touch location
+
+cat $1 | cut -c 20- | awk '$0="https://api.github.com/users/"$0' > url
+
+cat url | while read -r line; do
+    NAME=$(echo $line | cut -f5 -d'/')
+    RAW=rawfetch-$NAME
+    echo $line
+    echo $NAME
+    if [ ! -f $RAW ];then
+        curl --silent -H "Accept: application/vnd.github.v3+json" $line > $RAW
+        bash ${GETLOCATION}github-valid.sh $RAW "bash get-location.sh <your-list>" || exit 1
+    fi
+    cat $RAW | grep location | cut -c 15- | sed 's/,//' >> location
+done
+
+paste $1 $location > ../location-$FILE
