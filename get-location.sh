26f3621fa0ab7aa941b8240d6094bf15fa203fe0
tlani <familyquerida@gmail.com>
2021-04-19 18:05:15 -0500

    logic fix

--- get-location.sh
+++ get-location.sh
@@ -14,13 +14,13 @@ cat $1 | cut -c 20- | awk '$0="https://api.github.com/users/"$0' > url
 cat url | while read -r line; do
     NAME=$(echo $line | cut -f5 -d'/')
     RAW=rawfetch-$NAME
-    echo $line
+    #echo $line
     echo $NAME
     if [ ! -f $RAW ];then
         curl --silent -H "Accept: application/vnd.github.v3+json" $line > $RAW
-        bash ${GETLOCATION}github-valid.sh $RAW "bash get-location.sh <your-list>" || exit 1
+        bash ${GETLOCATION}github-valid.sh $RAW "bash get-location.sh $FILE" || exit 1
     fi
-    cat $RAW | grep location | cut -c 15- | sed 's/,//' >> location
+    cat $RAW | grep -m 1 location | cut -c 15- | sed 's/,//' >> location
 done
 
 paste $1 $location > ../location-$FILE
